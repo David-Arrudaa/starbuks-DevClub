708385adbbfa1453e4deee571a594836836abd8c
David_Arruda <david.viniciusarruda@gmail.com>
2025-07-15 19:12:53 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,11 @@
+let circulo = document.querySelector(".circulo")
+let imagem = document.querySelector(".startbuks")
+
+
+function trocarCor(cor){
+     circulo.style.backgroundColor = cor;
+}
+
+function trocarImagem(img){
+     imagem.src = img
+}
